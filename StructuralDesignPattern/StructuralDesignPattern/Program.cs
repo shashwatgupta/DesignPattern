4b11fdb8eb6f9fea9f744f382cc89cd1713d39e2
Shashwat Gupta <shashwat.gupta@gmail.com>
2018-06-01 23:41:34 -0700

    Add adapter Pattern

---
+++ StructuralDesignPattern/StructuralDesignPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuralDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adapter.Run();
+        }
+    }
+}
